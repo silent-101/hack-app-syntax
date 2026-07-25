9cc8de4adeb2a7fba05315507f9c23f5022a6624
silent-101 <developersilent101@gmail.com>
2026-07-25 16:57:46 +0000

    first commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,62 @@
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
+import appCss from '../styles.css?url'
+
+import type { QueryClient } from '@tanstack/react-query'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'HackApp',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+  shellComponent: RootDocument,
+})
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        {/* <TanStackDevtools */}
+        {/*   config={{ */}
+        {/*     position: 'bottom-right', */}
+        {/*   }} */}
+        {/*   plugins={[ */}
+        {/*     { */}
+        {/*       name: 'Tanstack Router', */}
+        {/*       render: <TanStackRouterDevtoolsPanel />, */}
+        {/*     }, */}
+        {/*     TanStackQueryDevtools, */}
+        {/*   ]} */}
+        {/* /> */}
+        <Scripts />
+      </body>
+    </html>
+  )
+}
